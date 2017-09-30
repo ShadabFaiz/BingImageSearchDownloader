6349815f740777495d66ca6a3eb22ee40245a4b2
ShadabFaiz <shadab.badshah@gmail.com>
2017-09-30 16:38:47 +0530

    Fixed the issue where certains keyword suc "sddfs", "sffsfdfdsf", were causing infinite loop.

--- Main_V2.py
+++ Main_V2.py
@@ -20,7 +20,7 @@ browser = webdriver.Chrome("F:\\Drivers\\Chrome\\chromedriver.exe")
 Bing_ImageSearch_url = ["https://www.bing.com/images/search?q=",
                         "&qft=+filterui:imagesize-custom"]
 
-img_to_search = "Cool"
+img_to_search = "werwerfsferterte"
 img_size = ["_640_480", "_800_600", "_1024_768", "_1600_1200", "_2272_1704", "_2816_2112"]
 image_quantity_required = 20
 
@@ -33,16 +33,23 @@ browser.get(Bing_ImageSearch_url[0] + img_to_search + Bing_ImageSearch_url[1]
 print(Bing_ImageSearch_url[0] + img_to_search + Bing_ImageSearch_url[1] + img_size[3])
 print(browser.title)
 img_found = False
-while img_found == False:
+refresh_counter = 10
+while (img_found is not True):
     try:
         # Finding the div tag of first image in the result
         first_div_class = browser.find_element_by_xpath("//ul/li/div/div/a/div[@class=\"img_cont hoff\"]")
         img_found = True
     except NoSuchElementException:
 
-        # If not image is foound for the given word, thein it will refresh the browser.
-        print("No image found.\nRefreshing Page")
-        browser.refresh()
+        # If refresh counter reaches to zero,then exit the program.
+        if(refresh_counter == 0):
+            print("No images could be found.\nExisting program now.")
+            raise SystemExit
+        else:
+            # Otherwise refresh the page.
+            print("No image found.\nRefreshing Page")
+            browser.refresh()
+            refresh_counter -= 1
 
 
 # Clicking the the fisrt image in the result.
@@ -59,10 +66,8 @@ else:
 
 waitTime = 10
 
-# Waiting for the image original size to appear.
-
 # Continue downloading images untill we get required number of images.
-while(counter < image_quantity_required):
+while(counter <= image_quantity_required):
     try:
         element = WebDriverWait(browser, waitTime).until(EC.presence_of_element_located(
             (By.XPATH, "//img[@class=\"mainImage accessible nofocus\"]")))
@@ -88,7 +93,7 @@ while(counter < image_quantity_required):
 
         # Click on "Next img button.."
         ActionChains(browser).move_to_element(next_image_btn).click(next_image_btn).perform()
-        time.sleep(5)
+        # time.sleep(5)
     except TimeoutException:
         print("Trying to refresh the page due to TimeoutException.")
         browser.refresh()
